1429a17539af552c9fc33c587ede5052f0d9f18a
ErikGraciosa <erikgraciosa@gmail.com>
2021-02-08 15:24:12 -0800

    building out file tree

---
+++ src/components/allCharacters/AllCharacters.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllCharacters = () => {
+  return (
+    <div>
+      <h1>The AllCharacters Component</h1>
+    </div>
+  );
+};
+
+export default AllCharacters;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <p>Welcome to Dimension C-132</p>
+    </div>
+  );
+};
+
+export default Header;
