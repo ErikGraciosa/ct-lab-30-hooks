8ff067db8b399302b40b6176d20a7f7e8c3a25d2
ErikGraciosa <erikgraciosa@gmail.com>
2021-02-09 07:50:26 -0800

    quotes coming back and in state

--- src/containers/allCharacters/AllCharacters.jsx
+++ src/containers/allCharacters/AllCharacters.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import Character from '../../components/character/Character';
+import { getAllCharacters } from '../../services/rickAndMortyApi';
 
 const AllCharacters = () => {
   const [loading, setLoading] = useState(true); //initialize loading to true
   const [quotes, setQuotes] = useState([]); //initialize quotes to empty string
 
   useEffect(() => {
-    const character = fetch('https://rickandmortyapi.com/api/character/')
-      .then(res => res.json());
-    console.log(character);
-    setLoading(false);
-
+    getAllCharacters()
+      .then((quotes) => {
+        setQuotes(quotes.results);
+        setLoading(false);
+      });
   }, []);
+
+  console.log(loading);
+  console.log(quotes);
 
   return (
     <div>
