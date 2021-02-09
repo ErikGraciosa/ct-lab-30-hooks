74f4a44a1330d5cd29f56ee1d6bf2c58894d45a0
ErikGraciosa <erikgraciosa@gmail.com>
2021-02-09 07:36:07 -0800

    useEffect fetching character list

--- src/containers/allCharacters/AllCharacters.jsx
+++ src/containers/allCharacters/AllCharacters.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Character from '../../components/character/Character';
 
 const AllCharacters = () => {
+  const [loading, setLoading] = useState(true); //initialize loading to true
+  const [quotes, setQuotes] = useState([]); //initialize quotes to empty string
 
+  useEffect(() => {
+    const character = fetch('https://rickandmortyapi.com/api/character/')
+      .then(res => res.json());
+    console.log(character);
+    setLoading(false);
 
-  
+  }, []);
+
   return (
     <div>
       <h1>Placeholder All Chars</h1>
